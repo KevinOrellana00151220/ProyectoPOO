46199387fba8cc46d38100a4e03710987ead7298
KevinOrellana00151220 <62663311+KevinOrellana00151220@users.noreply.github.com>
2021-06-23 19:37:38 -0600

    Login casi terminado

---
+++ Proyecto/Models/Symptom.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Proyecto.Models
+{
+    public partial class Symptom
+    {
+        public Symptom()
+        {
+            Obserbations = new HashSet<Obserbation>();
+        }
+
+        public int Id { get; set; }
+        public string CommonName { get; set; }
+        public int ReactionTime { get; set; }
+
+        public virtual ICollection<Obserbation> Obserbations { get; set; }
+    }
+}
